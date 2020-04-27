eec07a6d99a60f42b978fe49b31af3f17034fd88
Barabanov <stanislav.barabanov@getwizer.com>
2020-04-27 21:00:39 +0300

    WIP

--- espnet/espnet/asr/pytorch_backend/recog.py
+++ espnet/espnet/asr/pytorch_backend/recog.py
@@ -20,7 +20,6 @@ from espnet.utils.io_utils import LoadInputsAndTargets
 
 
 def recog_v2(args):
-    logging.info('Starting inference')
 
     """Decode with custom models that implements ScorerInterface.
 
@@ -111,6 +110,7 @@ def recog_v2(args):
             feat = load_inputs_and_targets(batch)[0][0]
             enc = model.encode(torch.as_tensor(feat).to(device=device, dtype=dtype))
             nbest_hyps = beam_search(x=enc, maxlenratio=args.maxlenratio, minlenratio=args.minlenratio)
+            logging.info()
             nbest_hyps = [h.asdict() for h in nbest_hyps[:min(len(nbest_hyps), args.nbest)]]
             new_js[name] = add_results_to_json(js[name], nbest_hyps, train_args.char_list)
 
