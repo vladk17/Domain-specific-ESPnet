a00355e4caaaaf35c9d148f13bb57c7056cef4ca
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-04 15:42:55 +0300

    continue to adopt datset to kaldi format

--- dataset_preparation/transformers/spanish_mailabs.py
+++ dataset_preparation/transformers/spanish_mailabs.py
@@ -1,5 +1,8 @@
 import os
+from distutils.dir_util import copy_tree
 from pathlib import Path
+from typing import List
+
 from pydub import AudioSegment
 from tqdm import tqdm
 import pandas as pd
@@ -12,12 +15,21 @@ class MailabsKaldiTransformer(AbstractDataTransformer):
 
         kaldi_data_dir = os.path.join(espnet_kaldi_eg_directory, 'data')
         kaldi_audio_files_dir = os.path.join(espnet_kaldi_eg_directory, 'downloads')
-        origin_audiofiles_dir = os.path.join(raw_data_path, 'clips')
+        root_dir = os.path.join(raw_data_path, 'es_ES', 'by_book')
 
-        data = pd.read_csv(Path(raw_data_path, 'validated.tsv'), delimiter='\t')
+        audio_dirs, transcript_paths, speakers = self.get_data_dirs(root_dir)
+
+        dfs = list()
+        for idx, transcript_path in enumerate(transcript_paths):
+            df = pd.read_csv(transcript_path, delimiter="|", error_bad_lines=False, engine='python',
+                             warn_bad_lines=False)
+            dfs.append(df)
+
+        data = pd.concat(dfs)
         dataset_size = data.shape[0]
-
         print("Total dataset size", dataset_size)
+
+        self.copy_audio_files_to_kaldi_dir(origin_paths=audio_dirs, destination_path=kaldi_audio_files_dir)
 
         if subset_size:
             print("Subset size:", subset_size)
@@ -29,35 +41,52 @@ class MailabsKaldiTransformer(AbstractDataTransformer):
             self.SUBSET_SIZE = subset_size
             data = data[:subset_size]
 
-        audio_files = data['path'].tolist()
-        if os.path.exists(kaldi_audio_files_dir):
+        # print("Generating data")
+        # wavscp, text, utt2spk = self.generate_arrays(data)
+        # self.create_files(wavscp, text, utt2spk, os.path.join(kaldi_data_dir, 'train_test'))
+
+    def copy_audio_files_to_kaldi_dir(self, origin_paths: List[str], destination_path):
+
+        print("Copying audio files to kaldi downloads directory...")
+        if os.path.exists(destination_path):
             pass
         else:
-            print("Transforming audio to .wav and copying to eg directory")
-            os.makedirs(kaldi_audio_files_dir)
-        for file in tqdm(audio_files):
-            joined_path = os.path.join(origin_audiofiles_dir, file)
-            self.convert_to_wav_from_mp3(joined_path, kaldi_audio_files_dir)
+            os.makedirs(destination_path)
+        for path in origin_paths:
+            copy_tree(path, destination_path)
 
-        print("Generating train and test files")
+    def get_data_dirs(self, root_dir):
+        gender_dirs = ['female', 'male']
 
-        wavscp, text, utt2spk = self.generate_arrays(data)
+        audio_dirs = []
+        transcript_paths = []
+        speakers = []
 
-        wavscp_train, wavscp_test, text_train, text_test, utt2spk_train, utt2spk_test = \
-            self.split_train_test(wavscp,
-                                  text,
-                                  utt2spk,
-                                  test_proportion=0.2)
+        for gender_dir in gender_dirs:
+            curdir = Path(root_dir, gender_dir)
+            speaker_dirs = list(os.walk(curdir))[0][1]
+            for idx, speaker_dir in enumerate(speaker_dirs):
+                book_dirs = list(os.walk(Path(curdir, speaker_dir)))[0][1]
+                for book_dir in book_dirs:
+                    final_abs_path = Path(curdir, speaker_dir, book_dir).absolute()
+                    audio_path = Path(final_abs_path, 'wavs')
+                    transcript_path = Path(final_abs_path, 'metadata.csv')
+                    audio_dirs.append(audio_path)
+                    transcript_paths.append(transcript_path)
+                    speakers.append(idx)
 
-        self.create_files(wavscp_train, text_train, utt2spk_train, os.path.join(kaldi_data_dir, 'train'))
-        self.create_files(wavscp_test, text_test, utt2spk_test, os.path.join(kaldi_data_dir, 'test'))
+        mix_dir = Path(root_dir, "mix")
+        mix_book_dirs = list(os.walk(mix_dir))[0][1]
 
-    def convert_to_wav_from_mp3(self, source_path: str, destination_folder: str):
+        for book_dir in mix_book_dirs:
+            final_abs_path = Path(mix_dir, book_dir).absolute()
+            audio_path = Path(final_abs_path, 'wavs')
+            transcript_path = Path(final_abs_path, 'metadata.csv')
+            audio_dirs.append(audio_path)
+            transcript_paths.append(transcript_path)
+            speakers.append(999)
 
-        new_file_name = source_path.split("/")[-1][:-4] + '.wav'
-        destination_path = Path(destination_folder, new_file_name)
-        sound = AudioSegment.from_mp3(source_path)
-        sound.export(destination_path, format="wav")
+        return audio_dirs, transcript_paths, speakers
 
     def generate_arrays(self, data: pd.DataFrame):
 
