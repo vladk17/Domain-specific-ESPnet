e409e737749ce69c9e5c5c5129e89aebc499e5d6
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-11 13:45:33 +0300

    WIP

--- espnet/egs/spanish_mailabs/asr1/local/utils/prepare_data.py
+++ espnet/egs/spanish_mailabs/asr1/local/utils/prepare_data.py
@@ -5,7 +5,7 @@ import os
 espnet_kaldi_eg_directory = '..'
 dataset_url = 'https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10/es.tar.gz'
 dataset_name = 'CommonVoiceSpanish'
-download_folder = 'raw_data'
+download_folder = '../raw_data'
 subset_size = int(os.environ.get('ESPNET_SUBSET_SIZE'))
 
 if __name__ == '__main__':
