90ad152b39b58a50a93d4f124b68ca5ab7c238ca
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-13 13:42:40 +0300

    code refactor

--- espnet/egs/spanish_crowdsource/asr1/local/make_data.py
+++ espnet/egs/spanish_crowdsource/asr1/local/make_data.py
@@ -2,17 +2,17 @@ from pathlib import Path
 from dataset_utils.dataset_downloader import download_and_extract_data
 from dataset_utils.transformers.spanish_crowdsource_openasr import CrowdsourcedOpenASR
 
+EG_NAME = 'spanish_crowdsource'
 dataset_urls = ['http://www.openslr.org/resources/71/es_cl_female.zip',
                 'http://www.openslr.org/resources/71/es_cl_male.zip'
                 ]
-dataset_name = 'crowdsource_chilean'
-eg_dir = Path('/espnet/egs/spanish_crowdsource/asr1')
+eg_dir = Path(f'/espnet/egs/{EG_NAME}/asr1')
 raw_data_folder = Path(eg_dir, 'raw_data')
 
 if __name__ == '__main__':
     dataset_location = download_and_extract_data(
         dataset_urls=dataset_urls,
-        dataset_name=dataset_name,
+        dataset_name=EG_NAME,
         download_folder=raw_data_folder,
         force_decompress=True)
 
