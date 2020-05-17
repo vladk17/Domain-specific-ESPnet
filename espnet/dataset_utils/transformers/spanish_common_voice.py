bf74f83258a753475362cc5781496b6bc41aa681
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-17 22:25:12 +0300

    take subset for common voice

--- espnet/dataset_utils/transformers/spanish_common_voice.py
+++ espnet/dataset_utils/transformers/spanish_common_voice.py
@@ -47,6 +47,8 @@ class CommonVoiceKaldiTransformer(AbstractDataTransformer):
             print("Transforming audio to .wav and copying to eg directory")
             os.makedirs(kaldi_audio_files_dir)
 
+        audio_files = audio_files[:1000]
+
         for a_path in tqdm(audio_files):
             self.convert_to_wav_from_mp3(a_path)
 
