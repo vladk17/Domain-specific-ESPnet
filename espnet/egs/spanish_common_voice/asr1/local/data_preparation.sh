396b03b9074fa93b58a02aeb9cf152babdbff356
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-17 22:39:29 +0300

    add downsampling to common voice

--- espnet/egs/spanish_common_voice/asr1/local/data_preparation.sh
+++ espnet/egs/spanish_common_voice/asr1/local/data_preparation.sh
@@ -9,5 +9,20 @@ apt install -y ffmpeg
 
 python3 make_data.py || exit 1
 
+echo "Downsampling files to 16000 kHz"
+
+cd ..
+mkdir -p resampled_downloads
+cd downloads/
+
+for file in *.wav
+do
+    sox $file -r 16000 "../resampled_downloads/${file}"
+done
+
+cd ..
+rm -rf downloads
+mv resampled_downloads downloads
+
 chmod -R 777 ../data
 chmod -R 777 ../downloads
