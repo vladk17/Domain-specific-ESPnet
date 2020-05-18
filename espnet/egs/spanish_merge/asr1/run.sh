26820ba027c0ed7b8c94b87d21667f6dea7027cb
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-18 18:40:42 +0300

    fix com voice

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -74,6 +74,8 @@ recog_set="test"
 
 train_dev_proportion=0.1
 
+mv data/test_comvoice data/test
+mv data/train_comvoice data/train
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
    ### Task dependent. You have to make data the following preparation part by yourself.
