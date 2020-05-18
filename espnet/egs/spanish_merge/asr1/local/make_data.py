8e631cd06e9de44d186fbb7bfa6b6b4ccffa8a49
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-18 14:59:31 +0300

    fix

--- espnet/egs/spanish_merge/asr1/local/make_data.py
+++ espnet/egs/spanish_merge/asr1/local/make_data.py
@@ -10,15 +10,22 @@ from collections import namedtuple
 
 DataSet = namedtuple('DataSet', ['name', 'urls', 'transformer_class'])
 
-datasets = [('Mailabs', ['http://www.caito.de/data/Training/stt_tts/es_ES.tgz'], MailabsKaldiTransformer),
+datasets = [('Mailabs', ['http://www.caito.de/data/Training/stt_tts/es_ES.tgz'], MailabsKaldiTransformer()),
             ('CommonVoiceSpanish', [
                 'https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10/es.tar.gz'],
-             CommonVoiceKaldiTransformer),
+             CommonVoiceKaldiTransformer()),
             ('TEDxSpanish', ['http://www.openslr.org/resources/67/tedx_spanish_corpus.tgz'],
-             TEDxSpanish2KaldiTransformer),
+             TEDxSpanish2KaldiTransformer()),
             ('Crowdsource', ['http://www.openslr.org/resources/71/es_cl_female.zip',
-                             'http://www.openslr.org/resources/71/es_cl_male.zip'
-                             ], CrowdsourcedOpenASR)]
+                             'http://www.openslr.org/resources/71/es_cl_male.zip',
+                             'http://www.openslr.org/resources/72/es_co_female.zip',
+                             'http://www.openslr.org/resources/72/es_co_male.zip',
+                             'http://www.openslr.org/resources/73/es_pe_female.zip',
+                             'http://www.openslr.org/resources/73/es_pe_male.zip',
+                             'http://www.openslr.org/resources/74/es_pr_female.zip',
+                             'http://www.openslr.org/resources/75/es_ve_female.zip',
+                             'http://www.openslr.org/resources/75/es_ve_male.zip'
+                             ], CrowdsourcedOpenASR())]
 datasets = [DataSet(_[0], _[1], _[2]) for _ in datasets]
 eg_dir = Path('/espnet/egs/spanish_merge/asr1')
 raw_data_folder = Path(eg_dir, 'raw_data')
@@ -26,7 +33,6 @@ raw_data_folder = Path(eg_dir, 'raw_data')
 
 def run_factory(datasets: List[DataSet]):
     for dataset in datasets:
-
         print(f"\nDownloading and extracting data for '{dataset.name}' dataset\n")
 
         dataset_location = download_and_extract_data(
