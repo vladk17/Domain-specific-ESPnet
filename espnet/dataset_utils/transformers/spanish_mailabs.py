94b053f010eae34bbbbc3515c0ddd767830210b5
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-21 14:31:34 +0300

    WIP

--- espnet/dataset_utils/transformers/spanish_mailabs.py
+++ espnet/dataset_utils/transformers/spanish_mailabs.py
@@ -111,7 +111,7 @@ class MailabsKaldiTransformer(AbstractDataTransformer):
             speaker_id = f"speaker{row['speaker']}"
 
             utt_id = idx+1
-            utterance_id = f'sp{self.prefix}{speaker_id}-ut{self.prefix}{utt_id}'
+            utterance_id = f'{self.prefix}_{speaker_id}-{self.prefix}_{utt_id}'
             wavscp.append(f'{utterance_id} {file_path}')
             if row['speaker'] == -1:
                 utt2spk.append(f'{utterance_id} {utterance_id}')
