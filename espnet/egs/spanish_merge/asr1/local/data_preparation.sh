4b665efcbe0f265a163cb1ddf8eb8bc3ca8e1e08
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-21 18:32:58 +0300

    fix wav

--- espnet/egs/spanish_merge/asr1/local/data_preparation.sh
+++ espnet/egs/spanish_merge/asr1/local/data_preparation.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-#cd "$(dirname "$0")"
-#pip install -r requirements.txt
-#
-#add-apt-repository ppa:mc3man/trusty-media
-#apt-get -y update
-#apt install -y ffmpeg
-#
-#python3 make_data.py
-#
-#echo "Downsampling files to 16000 kHz"
-#
-#cd ..
-#mkdir -p resampled_crowdsource
-#cd downloads/crowdsource
-#
-#for file in *.wav
-#do
-#    sox $file -r 16000 "../../resampled_crowdsource/${file}"
-#done
-#
-#cd ../..
-#
-#rm -rf downloads/crowdsource
-#mv resampled_crowdsource downloads/crowdsource
-#
-#chmod -R 777 data
-#chmod -R 777 downloads
+cd "$(dirname "$0")"
+pip install -r requirements.txt
+
+add-apt-repository ppa:mc3man/trusty-media
+apt-get -y update
+apt install -y ffmpeg
+
+python3 make_data.py
+
+echo "Downsampling files to 16000 kHz"
+
+cd ..
+mkdir -p resampled_crowdsource
+cd downloads/crowdsource
+
+for file in *.wav
+do
+    sox $file -r 16000 "../../resampled_crowdsource/${file}"
+done
+
+cd ../..
+
+rm -rf downloads/crowdsource
+mv resampled_crowdsource downloads/crowdsource
+
+chmod -R 777 data
+chmod -R 777 downloads
 
