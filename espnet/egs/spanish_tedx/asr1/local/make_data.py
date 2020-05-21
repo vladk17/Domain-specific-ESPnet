416ea1a5a0242be4f23433fe78cb9ad03e022c6a
Barabanov <stanislav.barabanov@getwizer.com>
2020-05-21 13:42:12 +0300

    add logger

--- espnet/egs/spanish_tedx/asr1/local/make_data.py
+++ espnet/egs/spanish_tedx/asr1/local/make_data.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from dataset_utils.dataset_downloader import download_and_extract_data
 from dataset_utils.transformers.spanish_tedx import TEDxSpanish2KaldiTransformer
+import logging
+
+logger = logging.root
+logger.addHandler(logging.StreamHandler)
+logger.setLevel(logging.INFO)
 
 dataset_urls = ['http://www.openslr.org/resources/67/tedx_spanish_corpus.tgz']
 dataset_name = 'spanish_tedx'
@@ -13,7 +18,7 @@ if __name__ == '__main__':
         dataset_name=dataset_name,
         download_folder=raw_data_folder)
 
-    print("Dataset location:", dataset_location)
+    logger.info("Dataset location:", dataset_location)
 
     transformer = TEDxSpanish2KaldiTransformer()
     transformer.transform(
