9cefb167b88313a79902aef927b2b53b90b6cb3e
Stanislav Barabanov <stsbar@gmail.com>
2020-06-04 18:58:59 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -8,8 +8,8 @@
 
 # general configuration
 backend=pytorch
-stage=0     # start from -1 if you need to start from data download
-stop_stage=2
+stage=5     # start from -1 if you need to start from data download
+stop_stage=100
 ngpu=4         # number of gpus ("0" uses cpu, otherwise use gpu)
 nj=32
 debugmode=1
