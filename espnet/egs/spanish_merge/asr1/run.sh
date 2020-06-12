63f4007eabc7a39f5774abd5a35e92fb6c1b40da
Stanislav Barabanov <stsbar@gmail.com>
2020-06-12 11:44:28 +0300

    fix LM preparation

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -187,14 +187,17 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
         mkdir -p ${lmdatadir}
 
         # merge trainset with unsupervised gong data
-        touch data/local/${lmbigname}
-        for i in data/${train_set}/text data/train_gong_unsupervised/text data/test_gong_unsupervised/text
-        do
-            cut -f 2- -d" " $i >> data/local/${lmbigname}
-        done
+#        touch data/local/${lmbigname}
+#        for i in data/${train_set}/text data/train_gong_unsupervised/text data/test_gong_unsupervised/text
+#        do
+#            cut -f 2- -d" " $i >> data/local/${lmbigname}
+#        done
+#
+#        spm_encode --model=${bpemodel}.model --output_format=piece < \
+#        data/local/${lmbigname} > ${lmdatadir}/train.txt
 
-        spm_encode --model=${bpemodel}.model --output_format=piece < \
-        data/local/${lmbigname} > ${lmdatadir}/train.txt
+        cut -f 2- -d" " data/${train_set}/text | spm_encode --model=${bpemodel}.model --output_format=piece \
+        > ${lmdatadir}/train.txt
 
         cut -f 2- -d" " data/${train_dev}/text | spm_encode --model=${bpemodel}.model --output_format=piece \
         > ${lmdatadir}/valid.txt
