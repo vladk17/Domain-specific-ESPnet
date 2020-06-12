d0a57cd721f6ef7d8cada06154c9f7812dbf9c45
Stanislav Barabanov <stsbar@gmail.com>
2020-06-12 17:26:12 +0300

    add data perturb to stage 1

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -9,7 +9,7 @@
 # general configuration
 backend=pytorch
 stage=1   # start from -1 if you need to start from data download
-stop_stage=3
+stop_stage=2
 ngpu=4         # number of gpus ("0" uses cpu, otherwise use gpu)
 nj=32
 debugmode=1
@@ -121,6 +121,16 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${lm_train_set}_org data/${lm_train_set}
 
     rm -rf data/${train_set}_org data/${train_dev}_org data/${recog_set}_org data/${lm_train_set}_org
+
+    # speed-perturbed
+    utils/perturb_data_dir_speed.sh 0.9 data/train data/temp1
+    utils/perturb_data_dir_speed.sh 1.0 data/train data/temp2
+    utils/perturb_data_dir_speed.sh 1.1 data/train data/temp3
+    utils/combine_data.sh --extra-files utt2uniq data/${train_set} data/temp1 data/temp2 data/temp3
+    rm -r data/temp1 data/temp2 data/temp3
+    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 --write_utt2num_frames true \
+        data/${train_set} exp/make_fbank/${train_set} ${fbankdir}
+    utils/fix_data_dir.sh data/${train_set}
 
     # compute global CMVN
     compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
