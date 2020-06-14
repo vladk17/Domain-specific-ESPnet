4ab8ac41fdd84001b788c4f2e31dbfd346f23801
Stanislav Barabanov <stsbar@gmail.com>
2020-06-14 13:54:58 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/local/make_data.py
+++ espnet/egs/spanish_merge/asr1/local/make_data.py
@@ -3,9 +3,11 @@ import os
 from pathlib import Path
 from typing import List
 
-from dataset_utils.dataset_downloader import download_and_extract_data, download_from_s3
+from dataset_utils.dataset_downloader import download_and_extract_data, download_from_s3, \
+    download_and_extract_data_from_kaggle_datasets
 from dataset_utils.transformers.spanish_gong import GongSpanish2KaldiTransformer
 from dataset_utils.transformers.spanish_gong_unsupervised import GongUnsupervisedSpanish2KaldiTransformer
+from dataset_utils.transformers.spanish_kaggle_120h import Kaggle120hSpanish2KaldiTransformer
 from dataset_utils.transformers.spanish_mailabs import MailabsKaldiTransformer
 from dataset_utils.transformers.spanish_common_voice import CommonVoiceKaldiTransformer
 from dataset_utils.transformers.spanish_tedx import TEDxSpanish2KaldiTransformer
@@ -19,7 +21,8 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 datasets = [
-    ('Mailabs', ['http://www.caito.de/data/Training/stt_tts/es_ES.tgz'], MailabsKaldiTransformer()),
+    # ('Mailabs', ['http://www.caito.de/data/Training/stt_tts/es_ES.tgz'], MailabsKaldiTransformer()),
+
     # ('CommonVoiceSpanish', [
     #     'https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10/es.tar.gz'],
     #  CommonVoiceKaldiTransformer()),
@@ -35,7 +38,8 @@ datasets = [
     #                  'http://www.openslr.org/resources/75/es_ve_female.zip',
     #                  'http://www.openslr.org/resources/75/es_ve_male.zip'
     #
-    #                  ], CrowdsourcedOpenASR())
+    #                  ], CrowdsourcedOpenASR()),
+    ('kaggle_120h_spanish_speech', None, Kaggle120hSpanish2KaldiTransformer())
 ]
 datasets = [DataSet(_[0], _[1], _[2]) for _ in datasets]
 eg_dir = Path('/espnet/egs/spanish_merge/asr1')
@@ -46,10 +50,17 @@ def prepare_public_data_factory(datasets: List[DataSet]):
     for dataset in datasets:
         logger.info(f"\n\nDownloading and extracting data for '{dataset.name}' dataset\n\n")
 
-        dataset_location = download_and_extract_data(
-            dataset_urls=dataset.urls,
-            dataset_name=dataset.name,
-            download_folder=raw_data_folder)
+        if dataset.name == 'kaggle_120h':
+            dataset_location = download_and_extract_data_from_kaggle_datasets(
+                kuggle_dataset_name=r'carlfm01/120h-spanish-speech',
+                kuggle_archive_name=r'120h-spanish-speech.zip',
+                dataset_name=dataset.name,
+                download_folder=raw_data_folder)
+        else:
+            dataset_location = download_and_extract_data(
+                dataset_urls=dataset.urls,
+                dataset_name=dataset.name,
+                download_folder=raw_data_folder)
 
         logger.info(f"Dataset location: {dataset_location}")
         logger.info(f"Using class {dataset.transformer_class}")
