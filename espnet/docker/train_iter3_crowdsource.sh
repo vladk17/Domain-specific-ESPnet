21ff14d33369d32d76ac45f655c25262cc33c0c8
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 23:49:25 +0300

    WIP

--- espnet/docker/train_iter3_crowdsource.sh
+++ espnet/docker/train_iter3_crowdsource.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 0,1,2,3 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999 \
---docker_env "ITERATION_NAME=3_crowdsource,ITERATION_TRAIN=data/train_crowdsources,ITERATION_VAL=data/test_crowdsource"
+./run.sh --docker_gpu 0,1,2,3 --docker_egs spanish_merge/asr1 \
+--docker_env "ITERATION_NAME=3_crowdsource,ITERATION_TRAIN=data/train_crowdsources,ITERATION_VAL=data/test_crowdsource" \
+--ngpu 4 --stage 1 --stop_stage 999
--- espnet/docker/train_iter4_comvoice.sh
+++ espnet/docker/train_iter4_comvoice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999 \
---docker_env "ITERATION_NAME=4_comvoice,ITERATION_TRAIN=data/train_comvoice,ITERATION_VAL=data/test_comvoice"
+./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 \
+--docker_env "ITERATION_NAME=4_comvoice,ITERATION_TRAIN=data/train_comvoice,ITERATION_VAL=data/test_comvoice" \
+--ngpu 4 --stage 1 --stop_stage 999
