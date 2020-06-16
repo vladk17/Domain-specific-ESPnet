e3f00f5f1d3be670630bbe94145330abb380d763
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 23:03:16 +0300

    WIP

--- espnet/docker/train_iter5_tedx.sh
+++ espnet/docker/train_iter5_tedx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 8,9,10,11 --docker_egs spanish_merge/asr1 --ngpu 4 --iteration 5_tedx \
+./run.sh --docker_gpu 8,9,10,11 --docker_egs spanish_merge/asr1 --docker_env --ngpu 4 --iteration 5_tedx \
 --iteration_train_datasets "data/train_tedx" --iteration_val_datasets "data/test_tedx"
