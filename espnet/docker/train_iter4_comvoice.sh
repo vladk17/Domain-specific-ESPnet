c65127e3e9ab4fc80e60d38638c811cedee13837
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 23:35:44 +0300

    WIP

--- espnet/docker/train_iter4_comvoice.sh
+++ espnet/docker/train_iter4_comvoice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 --docker_env --ngpu 4 --iteration 4_comvoice \
---iteration_train_datasets "data/train_comvoice" --iteration_val_datasets "data/test_comvoice"
+./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999 \
+--docker_env "ITERATION_NAME=4_comvoice,ITERATION_TRAIN=data/train_comvoice,ITERATION_VAL=data/test_comvoice"
--- espnet/docker/train_iter5_tedx.sh
+++ espnet/docker/train_iter5_tedx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 8,9,10,11 --docker_egs spanish_merge/asr1 --docker_env --ngpu 4 --iteration 5_tedx \
---iteration_train_datasets "data/train_tedx" --iteration_val_datasets "data/test_tedx"
+./run.sh --docker_gpu 8,9,10,11 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999 \
+--docker_env "ITERATION_NAME=5_tedx,ITERATION_TRAIN=data/train_tedx,ITERATION_VAL=data/test_tedx"
