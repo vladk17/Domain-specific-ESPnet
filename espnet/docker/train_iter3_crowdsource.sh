173eb3ac13616c44a082f6665669b42516f1144a
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 22:37:00 +0300

    WIP

--- espnet/docker/train_iter3_crowdsource.sh
+++ espnet/docker/train_iter3_crowdsource.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 0,1,2,3 --docker_egs spanish_merge/asr1 --docker_env --ngpu 4 --iteration 3_crowdsource \
+./run.sh --docker_gpu 0,1,2,3 --docker_egs spanish_merge/asr1 --ngpu 4 --iteration 3_crowdsource \
 --iteration_train_datasets "data/train_crowdsource" --iteration_val_datasets "data/test_crowdsource"
--- espnet/docker/train_iter6_mailabs.sh
+++ espnet/docker/train_iter6_mailabs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 12,13,14,15 --docker_egs spanish_merge/asr1 --docker_env --ngpu 4 --iteration 6_mailabs \
+./run.sh --docker_gpu 12,13,14,15 --docker_egs spanish_merge/asr1 --ngpu 4 --iteration 6_mailabs \
 --iteration_train_datasets "data/train_mailabs" --iteration_val_datasets "data/test_mailabs"
