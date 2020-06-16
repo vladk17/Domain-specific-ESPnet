52cac8142e02c0f0e8e3c43d0861edbcf7d65c36
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 23:41:32 +0300

    WIP

--- espnet/docker/train_iter4_comvoice.sh
+++ espnet/docker/train_iter4_comvoice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999 \
+./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 1 --stop_stage 999
 --docker_env "ITERATION_NAME=4_comvoice,ITERATION_TRAIN=data/train_comvoice,ITERATION_VAL=data/test_comvoice"
--- espnet/docker/train_iter6_mailabs.sh
+++ espnet/docker/train_iter6_mailabs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 12,13,14,15 --docker_egs spanish_merge/asr1 --ngpu 4 --stage 0 --stop_stage 999 \
---docker_env "ITERATION_NAME=6_mailabs,ITERATION_TRAIN=data/train_mailabs,ITERATION_VAL=data/test_mailabs"
+./run.sh --docker_gpu 12,13,14,15 --docker_egs spanish_merge/asr1 --docker_env "ITERATION_NAME=6_mailabs,ITERATION_TRAIN=data/train_mailabs,ITERATION_VAL=data/test_mailabs" --ngpu 4 --stage 0 --stop_stage 999
