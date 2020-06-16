eb332fd2febce06ea869ad44b70ebf24e81379e4
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 17:51:34 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -106,8 +106,8 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     fbankdir=fbank
 
     # select datasets for train, dev, test. You can choose any dataset from "datasets" variable which was preprocessed earlier
-    utils/combine_data.sh  data/${train_set} data/train_crowdsource
-    utils/combine_data.sh  data/${train_dev} data/test_crowdsource
+    utils/combine_data.sh  data/${train_set} data/train_comvoice
+    utils/combine_data.sh  data/${train_dev} data/test_comvoice
     utils/combine_data.sh  data/${recog_set} data/test_gong data/train_gong
 
     # select datasets for LM only
