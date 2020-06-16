1a208b487ea521f21f2d29db7837da6ffcad73ad
Stanislav Barabanov <stsbar@gmail.com>
2020-06-17 00:21:28 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -124,7 +124,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
     for x in ${train_set} ${train_dev} ${recog_set}; do
         steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj ${nj} --write_utt2num_frames true \
-            data/${x}_org exp/make_fbank/${x} ${fbankdir}
+            data/${x} exp/make_fbank/${x} ${fbankdir}
         utils/fix_data_dir.sh data/${x}
     done
 
