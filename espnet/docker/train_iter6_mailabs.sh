72b684294ce378526a839542fe811546d9956d0d
Stanislav Barabanov <stsbar@gmail.com>
2020-06-16 23:47:36 +0300

    WIP

--- espnet/docker/train_iter6_mailabs.sh
+++ espnet/docker/train_iter6_mailabs.sh
@@ -2,4 +2,4 @@
 
 ./run.sh --docker_gpu 12,13,14,15 --docker_egs spanish_merge/asr1 \
 --docker_env "ITERATION_NAME=6_mailabs,ITERATION_TRAIN=data/train_mailabs,ITERATION_VAL=data/test_mailabs" \
---ngpu 4 --stage 1 --stop_stage 999
+--ngpu 4 --stage 1 --stop_stage 2
