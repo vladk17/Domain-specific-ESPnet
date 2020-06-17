45309b10f6391300fcba1a3ca7fa9670bac1456c
Stanislav Barabanov <stsbar@gmail.com>
2020-06-17 13:16:03 +0300

    WIP

--- espnet/docker/train_iter3_crowdsource.sh
+++ espnet/docker/train_iter3_crowdsource.sh
@@ -2,4 +2,4 @@
 
 ./run.sh --docker_gpu 0,1,2,3 --docker_egs spanish_merge/asr1 \
 --docker_env "ITERATION_NAME=3_crowdsource,ITERATION_TRAIN=data/train_crowdsource,ITERATION_VAL=data/test_crowdsource" \
---ngpu 4 --stage 5 --stop_stage 999
+--ngpu 4 --stage 1 --stop_stage 999
