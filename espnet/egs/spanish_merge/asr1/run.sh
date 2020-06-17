b02c20e3127bbc65d4baf893fa9eec1eb7dc3947
Stanislav Barabanov <stsbar@gmail.com>
2020-06-17 12:42:54 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -339,7 +339,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
             --result-label ${expdir}/${decode_dir}/data.JOB.json \
             --model ${expdir}/results/${recog_model}  \
             --api v2 \
-#            --rnnlm ${lmexpdir}/${lang_model}
+            --rnnlm ${lmexpdir}/${lang_model}
 
         score_sclite.sh --bpe ${nbpe} --bpemodel ${bpemodel}.model --wer true ${expdir}/${decode_dir} ${dict}
 
