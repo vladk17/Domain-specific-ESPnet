ddcdc93c4384edd0547b3b2394c7064449a8f547
Stanislav Barabanov <stsbar@gmail.com>
2020-06-17 11:13:33 +0300

    WIP

--- espnet/docker/train_iter4_comvoice.sh
+++ espnet/docker/train_iter4_comvoice.sh
@@ -2,4 +2,4 @@
 
 ./run.sh --docker_gpu 4,5,6,7 --docker_egs spanish_merge/asr1 \
 --docker_env "ITERATION_NAME=4_comvoice,ITERATION_TRAIN=data/train_comvoice,ITERATION_VAL=data/test_comvoice" \
---ngpu 4 --stage 1 --stop_stage 999
+--ngpu 4 --stage 5 --stop_stage 999
